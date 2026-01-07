f3167f6e11ff86c7a224d8a89ca119948e887979
Ayrton Nacer <hiayrtonnacer@gmail.com>
2026-01-07 17:42:05 -0300

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: '/secure-view-landing/,
+  base: '/secure-view-landing/',
   server: {
     host: "::",
     port: 8080,
